f7459c918560118366595c28399d5a1be02e5190
Rowland Kibet <rowkibet@gmail.com>
2021-08-08 23:17:27 +0300

    Added connect.php
    Created the connection object

---
+++ database/connect.php
@@ -0,0 +1,17 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db_name = 'rental management system';
+
+
+// This object creates a connection to the specified database
+$conn = new MySQLi($host, $user, $pass, $db_name);
+
+// To check if there is any error while establishing connection
+if ($conn->connect_error) {
+    die('Database connection error: ' . $conn->connect_error);// The app will stop executing
+} else {
+    echo "DB connection successful";    
+}
+?>
